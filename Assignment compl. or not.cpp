cec915abaf216cb6b3f211c04b957f1720b15705
Abhinandan Guruprasad  Madiwal <163436368+Abhinandan999@users.noreply.github.com>
2024-04-06 21:15:25 +0530

    Add files via upload

---
+++ Assignment compl. or not.cpp
@@ -0,0 +1,29 @@
+/*Janmansh has to submit 3
+3 assignments for Chingari before 10
+10 pm and he starts to do the assignments at �
+X pm. Each assignment takes him 
+1 hour to complete. Can you tell whether he'll be able to complete all assignments on time or not?*/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a,T;
+	cout<<""<<endl;
+	cin>>T;
+	
+	for(int i=1;i<=T;i++)
+	{
+	    cout<<""<<endl;
+	    cin>>a;
+	    
+	    if(a<=7){
+	        cout<<"Yes"<<endl;
+	    }
+	    else 
+	    cout<<"No"<<endl;
+	}
+
+}
