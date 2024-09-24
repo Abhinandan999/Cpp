b20586c688fe054bdc440aac3b27227e53d7927a
Abhinandan Guruprasad  Madiwal <163436368+Abhinandan999@users.noreply.github.com>
2024-09-24 23:59:53 +0530

    Add files via upload

---
+++ vector_basics.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    vector<int> var;
+
+    for(int i:var){
+        cout<<var<<" ";
+    }
+   
+      vec.push_back(25);
+        vec.push_back(35);
+        vec.push_back(45);
+        cout<<" Vector after pushing "<<endl;
+          for(int i:var){
+        cout<<var<<" ";
+    }
+        vec.pop_back();
+
+        cout<<" Vector after poping "<<endl;
+  for(int i:var){
+        cout<<var<<" ";
+    }
+    cout<<"First value:- "<<endl;
+    vec.front();
+    cout<<"Last valus\e:-"<<endl;
+    vec.back();
+    
+    cout<<"Value of vector at index 3:- "<<endl;
+    vec.at(9);
+
+    return 0;
+
+}
