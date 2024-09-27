b59cd7fb28724c160ac742c5bed5e6175e38aeb1
Abhinandan Guruprasad  Madiwal <163436368+Abhinandan999@users.noreply.github.com>
2024-09-27 23:50:12 +0530

    Add files via upload

---
+++ kadanes's_algorithm.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int n=5;
+int arr[5]={1,2,3,4,5};
+int currentsum=0;
+int maxSum=INT_MIN;//-infinity
+
+for(int i=0;i<n;i++){
+    for(int j=i;j<n-1;j++){
+        currentsum += arr[j];
+        maxSum = max(currentsum, maxSum);
+        if(currentsum<0){
+            currentsum=0;
+        }
+    }
+    cout<<endl;
+}
+cout<<"Maximum sum is: - "<<maxSum<<endl;
+return 0;
+
+}
